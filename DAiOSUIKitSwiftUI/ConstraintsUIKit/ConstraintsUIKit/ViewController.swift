74bd0ecb59626f4571635eb19b0740ae16b2e6d5
Marcos Avila <marcosa31@gmail.com>
2022-02-09 00:24:09 -0600

    Use of notificationCenter to catch when keyboard shows/hide and register callback to handle custom logic. Use of UIScreen nativebounds to conditionally   push the frame on Y axis in order to avoid be covered controls by the keyboard.

--- DAiOSUIKitSwiftUI/ConstraintsUIKit/ConstraintsUIKit/ViewController.swift
+++ DAiOSUIKitSwiftUI/ConstraintsUIKit/ConstraintsUIKit/ViewController.swift
@@ -12,8 +12,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoUp), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoDown), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func tecladoUp(){
+        // Retrieve height size of the bounds in iphone SE
+        let sizeScreen = UIScreen.main.nativeBounds.height
+        if sizeScreen == 1136 && self.view.frame.origin.y == 0 {
+            self.view.frame.origin.y = -50
+        }
+    }
+    
+    @objc func tecladoDown() {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 }
 
