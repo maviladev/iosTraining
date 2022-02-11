b20b61ea07e1f1e582ff8741b05328932fa49a09
Marcos Avila <marcosa31@gmail.com>
2022-02-10 22:23:28 -0600

    Initial Commit
    Initial structure without Storyboards
    LoginView

---
+++ SwiftArcadeiOSDev/Bankey/Bankey/Login/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Marcos Avila Rodriguez on 10/02/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    let loginView = LoginView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        style()
+        layout()
+    }
+}
+
+extension LoginViewController {
+    private func style() {
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+    private func layout() {
+        
+        view.addSubview(loginView)
+    
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
+        
+    }
+    
+}
+
