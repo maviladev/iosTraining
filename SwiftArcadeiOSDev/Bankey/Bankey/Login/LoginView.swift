6a3dd11c6e70ad33afbc78f03dc79801dc5af4ce
Marcos Avila <marcosa31@gmail.com>
2022-02-10 23:45:17 -0600

    Stackview and password field
    Added stackview to LoginView
    Moved usernameTextField to stackview
    Declared userPassTextField on LoginView and added to stackview

--- SwiftArcadeiOSDev/Bankey/Bankey/Login/LoginView.swift
+++ SwiftArcadeiOSDev/Bankey/Bankey/Login/LoginView.swift
@@ -11,6 +11,8 @@ import UIKit
 class LoginView: UIView, UITextFieldDelegate {
     
     let usernameTextField = UITextField()
+    let userPassTextField = UITextField()
+    let stackView = UIStackView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -21,10 +23,6 @@ class LoginView: UIView, UITextFieldDelegate {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    override var intrinsicContentSize: CGSize {
-        return CGSize(width: 200, height: 200)
     }
 }
 
@@ -38,22 +36,38 @@ extension LoginView {
         usernameTextField.placeholder = "Username"
         usernameTextField.delegate = self
         
+        userPassTextField.translatesAutoresizingMaskIntoConstraints = false
+        userPassTextField.placeholder = "Password"
+        userPassTextField.isSecureTextEntry = true
+        userPassTextField.delegate = self
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        
     }
     
     func layout() {
         
-        addSubview(usernameTextField)
+        stackView.addArrangedSubview(usernameTextField)
+        stackView.addArrangedSubview(userPassTextField)
+        
+        addSubview(stackView)
         
         NSLayoutConstraint.activate([
-            usernameTextField.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
-            usernameTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
-            trailingAnchor.constraint(equalToSystemSpacingAfter: usernameTextField.trailingAnchor, multiplier: 1)
+            stackView.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1),
+            bottomAnchor.constraint(equalToSystemSpacingBelow: stackView.bottomAnchor, multiplier: 1)
             
         ])
+        
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         usernameTextField.endEditing(true)
+        userPassTextField.endEditing(true)
+        
         return true
     }
     
