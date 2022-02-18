d5b73fc5018b655e97ac61d17643814e796f696a
Marcos Avila <marcosa31@gmail.com>
2022-02-18 16:59:19 -0600

    To put on correct flow
    Small changes to app delegate to ensure the correct login flow

--- SwiftArcadeiOSDev/Bankey/Bankey/AppDelegate.swift
+++ SwiftArcadeiOSDev/Bankey/Bankey/AppDelegate.swift
@@ -28,12 +28,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         loginViewController.delegate = self
         onboardingContainerViewController.delegate = self
         
-        let vc = mainViewController
-        vc.setStatusBar()
         
-        UINavigationBar.appearance().isTranslucent = false
-        UINavigationBar.appearance().backgroundColor = appColor
-        
+        displayLogin()
+        return true
 //        window?.rootViewController = loginViewController
 //        window?.rootViewController = onboardingContainerViewController
 //        window?.rootViewController = OnboardingContainerViewController()
@@ -41,9 +38,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
 //        window?.rootViewController = mainViewController
         
-        window?.rootViewController = vc
+        
 //        mainViewController.selectedIndex = 0,1,2,etc..
-        return true
+        
+    }
+    
+    private func displayLogin() {
+        setRootViewController(loginViewController)
+    }
+    
+    private func displayNextScreen() {
+        if LocalState.hasOnboarded {
+            prepMainView()
+            setRootViewController(mainViewController)
+        } else {
+            setRootViewController(onboardingContainerViewController)
+        }
+    }
+    
+    private func prepMainView() {
+        mainViewController.setStatusBar()
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().backgroundColor = appColor
     }
     
 }
@@ -54,15 +70,12 @@ extension AppDelegate: LoginViewControllerDelegate, OnboardingContainerViewContr
     }
     
     func didLogin() {
-        if LocalState.hasOnboarded {
-            setRootViewController(mainViewController)
-        } else {
-            setRootViewController(onboardingContainerViewController)
-        }
+        displayNextScreen()
     }
     
     func didFinishOnboarding() {
         LocalState.hasOnboarded = true
+        prepMainView()
         setRootViewController(mainViewController)
     }
 }
