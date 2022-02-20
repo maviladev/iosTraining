e1ebafadf2fdd37251ff367d0811c615182ccb57
Marcos Avila <marcosa31@gmail.com>
2022-02-20 01:13:16 -0600

    Animations
    Added animation to sign in button
    Bell image configuration and animation configuration

--- SwiftArcadeiOSDev/Bankey/Bankey/Login/LoginViewController.swift
+++ SwiftArcadeiOSDev/Bankey/Bankey/Login/LoginViewController.swift
@@ -164,7 +164,7 @@ extension LoginViewController {
             configureView(withMessage: "Username / password cannot be blank")
             return
         }
-        
+
         if username == "dev" && password == "yesIam" {
             // loader at side of title button
             signInButton.configuration?.showsActivityIndicator = true
@@ -182,7 +182,21 @@ extension LoginViewController {
     private func configureView(withMessage message: String){
         errorMessageLabel.isHidden = false
         errorMessageLabel.text = message
+        shakeButton()
     }
+    
+    private func shakeButton() {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 10, -10, 10, 0]
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1]
+        animation.duration = 0.4
+        
+        animation.isAdditive = true
+        signInButton.layer.add(animation, forKey: "shake")
+        
+    }
+    
 }
 
 // MARK: - Animations
