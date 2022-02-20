b339b37d6ece0cb90e484e5ecd39e6460b2c0dad
Marcos Avila <marcosa31@gmail.com>
2022-02-19 23:46:54 -0600

    Adjust font size in ipod 7
    Dynamically adjust the font to fit the view adjustsFontSizeToFitWidth.

--- SwiftArcadeiOSDev/Bankey/Bankey/AccountSummary/Cells/AccountSummaryCell.swift
+++ SwiftArcadeiOSDev/Bankey/Bankey/AccountSummary/Cells/AccountSummaryCell.swift
@@ -85,6 +85,7 @@ extension AccountSummaryCell {
         
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         nameLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        nameLabel.adjustsFontSizeToFitWidth = true
         nameLabel.text = "Account name"
         
         balanceStackView.translatesAutoresizingMaskIntoConstraints = false
@@ -94,6 +95,7 @@ extension AccountSummaryCell {
         balanceLabel.translatesAutoresizingMaskIntoConstraints = false
         balanceLabel.font = UIFont.preferredFont(forTextStyle: .body)
         balanceLabel.textAlignment = .right
+        balanceLabel.adjustsFontSizeToFitWidth = true
         balanceLabel.text = "Some balance"
         
         balanceAmountLabel.translatesAutoresizingMaskIntoConstraints = false
