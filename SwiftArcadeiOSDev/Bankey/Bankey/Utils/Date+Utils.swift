b60cfcad1d94b54d5b91662d3836afeec547645c
Marcos Avila <marcosa31@gmail.com>
2022-02-21 23:04:11 -0600

    Added viewmodel for header data
    Added date extension to format raw value

---
+++ SwiftArcadeiOSDev/Bankey/Bankey/Utils/Date+Utils.swift
@@ -0,0 +1,27 @@
+//
+//  Date+Utils.swift
+//  Bankey
+//
+//  Created by Marcos Avila Rodriguez on 21/02/22.
+//
+
+import Foundation
+
+
+extension Date {
+    static var bankeyDateFormatter: DateFormatter {
+        
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(abbreviation: "MDT")
+        return formatter
+        
+    }
+    
+    var monthDayYearString: String {
+        
+        let dateFormatter = Date.bankeyDateFormatter
+        dateFormatter.dateFormat = "MMM d, yyyy"
+        return dateFormatter.string(from: self)
+        
+    }
+}
