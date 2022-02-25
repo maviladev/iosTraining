b9afa56e7c633229344310ca568d21ce6abcfca3
Marcos Avila <marcosa31@gmail.com>
2022-02-25 12:20:33 -0600

    Mock data
    Initialize mock data and react to tap gesture

--- BuildBeautifulOnboardingi13S5/Onboarding Screens/Onboarding Screens/ViewController.swift
+++ BuildBeautifulOnboardingi13S5/Onboarding Screens/Onboarding Screens/ViewController.swift
@@ -25,13 +25,51 @@ class ViewController: UIViewController {
 //    Declare an empty array
 //    private let items: [OnboardingItems] = []
     
+    private let items: [OnboardingItems] = [
+        .init(title: "Travel Your Way",
+              detail: "Travel the world by air, rail or sea at the most competitive prices",
+              bgImage: nil),
+        .init(title: "Stay Your Way",
+              detail: "With over millions of hotels worldwide, find the perfect accomodation in the most amazing places!",
+              bgImage: nil),
+        .init(title: "Discover Your Way With New Features",
+              detail: "Explore exotic destinations with our new features that link you to like-minded travellers!",
+              bgImage: nil),
+        .init(title: "Feast Your Way",
+              detail: "we recommend you local cuisines that are safe and highly recommended by the locals!",
+              bgImage: nil)
+    ]
     
+    private let currentPage: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupPageControl()
+        setupScreen(index: currentPage)
+        setupTapGesture()
+        
+    }
+    
+    private func setupPageControl() {
+        pageControl.numberOfPages = items.count
+        
     }
 
+    private func setupScreen(index: Int) {
+        titleLabel.text = items[index].title
+        detailLabel.text = items[index].detail
+        pageControl.currentPage = index
+    }
 
+    private func setupTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapAnimation))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func handleTapAnimation(){
+        print("Tapped!")
+    }
+    
 }
 
