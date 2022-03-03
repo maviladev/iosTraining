40b3d6f2db0822f92c817647dc495802b2c9baae
Marcos Avila <marcosa31@gmail.com>
2022-03-02 23:14:44 -0600

    Handle get started button
    Hide and Show navigationbar
    Hide Status bar by overriding prefersStatusBarHidden

--- BuildBeautifulOnboardingi13S5/Onboarding Screens/HolidayApp/HolidayApp/ViewController.swift
+++ BuildBeautifulOnboardingi13S5/Onboarding Screens/HolidayApp/HolidayApp/ViewController.swift
@@ -15,9 +15,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupViews()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
 
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    private func setupViews(){
+        getStartedButton.layer.cornerRadius = 28
+        getStartedButton.layer.masksToBounds = true
+    }
+    
     @IBAction func getStartedButtonTapped(_ sender: Any) {
         
     }
