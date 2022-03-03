31deef04590e171e23f2f240b17b29374e4db49c
Marcos Avila <marcosa31@gmail.com>
2022-03-02 23:55:51 -0600

    Add background video

--- BuildBeautifulOnboardingi13S5/Onboarding Screens/HolidayApp/HolidayApp/ViewController.swift
+++ BuildBeautifulOnboardingi13S5/Onboarding Screens/HolidayApp/HolidayApp/ViewController.swift
@@ -6,11 +6,20 @@
 //
 
 import UIKit
+import AVFoundation
+import Combine
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var getStartedButton: UIButton!
     @IBOutlet weak var darkView: UIView!
+    
+    private var player: AVPlayer?
+    private var playerLayer: AVPlayerLayer?
+    
+    private let notificationCenter = NotificationCenter.default
+    private var appEventSuscribers = [AnyCancellable]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +30,9 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated)
+        observeAppEvents()
+        setupPlayerIfNeeded()
+        restartVideo()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -28,6 +40,11 @@ class ViewController: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        playerLayer?.frame = view.bounds
+    }
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
@@ -35,6 +52,53 @@ class ViewController: UIViewController {
     private func setupViews(){
         getStartedButton.layer.cornerRadius = 28
         getStartedButton.layer.masksToBounds = true
+        darkView.backgroundColor = UIColor(white: 0.1, alpha: 0.4)
+    }
+    
+    private func buildPlayer() -> AVPlayer? {
+        guard let filePath = Bundle.main.path(forResource: "bg_video", ofType: "mp4") else { return nil }
+        let url = URL(fileURLWithPath: filePath)
+        let player = AVPlayer(url: url)
+        player.actionAtItemEnd = .none
+        player.isMuted = false
+        return player
+    }
+    
+    private func buildPlayerLayer() -> AVPlayerLayer? {
+        let layer = AVPlayerLayer(player: player)
+        layer.videoGravity = .resizeAspectFill
+        return layer
+    }
+    
+    private func playVideo() {
+        player?.play()
+    }
+    
+    private func restartVideo() {
+        player?.seek(to: .zero)
+        playVideo()
+    }
+    
+    private func pauseVideo() {
+        player?.pause()
+    }
+    
+    private func setupPlayerIfNeeded(){
+        player = buildPlayer()
+        playerLayer = buildPlayerLayer()
+        
+        if let layer = self.playerLayer {
+            if view.layer.sublayers?.contains(layer) == false {
+            view.layer.insertSublayer(layer, at: 0)
+            }
+        }
+        
+    }
+    
+    private func observeAppEvents() {
+        notificationCenter.publisher(for: .AVPlayerItemDidPlayToEndTime).sink {[weak self] _ in
+            self?.restartVideo()
+        }.store(in: &appEventSuscribers)
     }
     
     @IBAction func getStartedButtonTapped(_ sender: Any) {
