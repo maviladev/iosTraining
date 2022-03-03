56a68477b0d738de260a8cba2330143385c91126
Marcos Avila <marcosa31@gmail.com>
2022-03-03 10:02:17 -0600

    Setup Onboarding CollectionView Cell

--- BuildBeautifulOnboardingi13S5/Onboarding Screens/RestaurantApp/RestaurantApp/OnboardingViewController.swift
+++ BuildBeautifulOnboardingi13S5/Onboarding Screens/RestaurantApp/RestaurantApp/OnboardingViewController.swift
@@ -7,10 +7,24 @@
 
 import UIKit
 
+struct Slide {
+    let title: String
+    let animationName: String
+    let buttonColor: UIColor
+    let buttonTitle: String
+    
+    static let collection: [Slide] = [
+        .init(title: "Get your favourite food delivered to you under 30 minutes anytime", animationName: "", buttonColor: .systemYellow, buttonTitle: "Next"),
+        .init(title: "We serve only from choiced restaurants in your area", animationName: "", buttonColor: .systemGreen, buttonTitle: "Order Now")
+            
+    ]
+}
+
 class OnboardingViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
+    private let slides: [Slide] = Slide.collection
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,12 +48,13 @@ extension OnboardingViewController: UICollectionViewDelegate,
                                     UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return slides.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
-        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath) as! OnboardingCollectionViewCell
+        let slide = slides[indexPath.item]
+        cell.configure(with: slide)
         return cell
     }
     
@@ -54,6 +69,25 @@ extension OnboardingViewController: UICollectionViewDelegate,
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
+    }
+}
+
+class OnboardingCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var animationView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var actionButton: UIButton!
+    
+    func configure(with slide: Slide) {
+        
+        titleLabel.text = slide.title
+        actionButton.backgroundColor = slide.buttonColor
+        actionButton.setTitle(slide.buttonTitle, for: .normal)
+    }
+    
+    
+    @IBAction func actionButtonTapped() {
+            
     }
 }
 
