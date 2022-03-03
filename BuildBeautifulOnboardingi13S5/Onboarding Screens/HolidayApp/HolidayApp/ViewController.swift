112fb022bfcf8fc0029642b5851463d267e66718
Marcos Avila <marcosa31@gmail.com>
2022-03-02 22:56:03 -0600

    Setting up the UI

---
+++ BuildBeautifulOnboardingi13S5/Onboarding Screens/HolidayApp/HolidayApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HolidayApp
+//
+//  Created by Marcos Avila Rodriguez on 02/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var getStartedButton: UIButton!
+    @IBOutlet weak var darkView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func getStartedButtonTapped(_ sender: Any) {
+        
+    }
+}
+
