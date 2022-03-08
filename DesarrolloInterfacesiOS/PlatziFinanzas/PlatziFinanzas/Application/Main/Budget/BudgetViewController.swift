e9f1180996f495d8e9e248669c231de2cd9d03dc
Marcos Avila <marcosa31@gmail.com>
2022-03-07 22:51:37 -0600

    Setup Budget view

--- DesarrolloInterfacesiOS/PlatziFinanzas/PlatziFinanzas/Application/Main/Budget/BudgetViewController.swift
+++ DesarrolloInterfacesiOS/PlatziFinanzas/PlatziFinanzas/Application/Main/Budget/BudgetViewController.swift
@@ -1,5 +1,5 @@
 //
-//  TransactionsViewController.swift
+//  BudgetViewController.swift
 //  PlatziFinanzas
 //
 //  Created by Marcos Avila Rodriguez on 07/03/22.
@@ -7,16 +7,9 @@
 
 import UIKit
 
-class TransactionsViewController: UIViewController {
+class BudgetViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    
-    fileprivate(set) lazy var emptyState: UIView = {
-        guard let view = Bundle.main.loadNibNamed("EmptyState", owner: nil, options: [:])?.first as? UIView else {
-            return UIView()
-        }
-        return view
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,21 +21,19 @@ class TransactionsViewController: UIViewController {
         let cell = UINib(nibName: "TransactionsCell", bundle: Bundle.main)
         tableView.register(cell, forCellReuseIdentifier: "cellid")
     }
-
 }
 
-extension TransactionsViewController: UITableViewDelegate {
+extension BudgetViewController: UITableViewDelegate {
     
 }
 
-extension TransactionsViewController: UITableViewDataSource {
+extension BudgetViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return tableView.dequeueReusableCell(withIdentifier: "cellid", for: indexPath)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let count = 10
-        tableView.backgroundView = count == 0 ? emptyState : nil
         tableView.separatorStyle = count == 0 ? .none : .singleLine
         return count
     }
